cc8010d01a6efa3363669ae58d08db97500573e2
Sebastian <280301@via.dk>
2022-01-09 19:56:08 +0100

    PANIC

--- AuthorBlazor/obj/Debug/net5.0/RazorDeclaration/Pages/CreateAuthor.razor.g.cs
+++ AuthorBlazor/obj/Debug/net5.0/RazorDeclaration/Pages/CreateAuthor.razor.g.cs
@@ -12,105 +12,105 @@ namespace AuthorBlazor.Pages
     using System.Linq;
     using System.Threading.Tasks;
 #nullable restore
-#line 1 "C:\Users\N_i_v\RiderProjects\Test-Exam-A20\AuthorBlazor\_Imports.razor"
+#line 1 "C:\Users\Frederik\RiderProjects\Test-Exam-A20\AuthorBlazor\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\N_i_v\RiderProjects\Test-Exam-A20\AuthorBlazor\_Imports.razor"
+#line 2 "C:\Users\Frederik\RiderProjects\Test-Exam-A20\AuthorBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\N_i_v\RiderProjects\Test-Exam-A20\AuthorBlazor\_Imports.razor"
+#line 3 "C:\Users\Frederik\RiderProjects\Test-Exam-A20\AuthorBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\N_i_v\RiderProjects\Test-Exam-A20\AuthorBlazor\_Imports.razor"
+#line 4 "C:\Users\Frederik\RiderProjects\Test-Exam-A20\AuthorBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\N_i_v\RiderProjects\Test-Exam-A20\AuthorBlazor\_Imports.razor"
+#line 5 "C:\Users\Frederik\RiderProjects\Test-Exam-A20\AuthorBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\N_i_v\RiderProjects\Test-Exam-A20\AuthorBlazor\_Imports.razor"
+#line 6 "C:\Users\Frederik\RiderProjects\Test-Exam-A20\AuthorBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\N_i_v\RiderProjects\Test-Exam-A20\AuthorBlazor\_Imports.razor"
+#line 7 "C:\Users\Frederik\RiderProjects\Test-Exam-A20\AuthorBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\N_i_v\RiderProjects\Test-Exam-A20\AuthorBlazor\_Imports.razor"
+#line 8 "C:\Users\Frederik\RiderProjects\Test-Exam-A20\AuthorBlazor\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\N_i_v\RiderProjects\Test-Exam-A20\AuthorBlazor\_Imports.razor"
+#line 9 "C:\Users\Frederik\RiderProjects\Test-Exam-A20\AuthorBlazor\_Imports.razor"
 using AuthorBlazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\N_i_v\RiderProjects\Test-Exam-A20\AuthorBlazor\_Imports.razor"
+#line 10 "C:\Users\Frederik\RiderProjects\Test-Exam-A20\AuthorBlazor\_Imports.razor"
 using AuthorBlazor.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\N_i_v\RiderProjects\Test-Exam-A20\AuthorBlazor\_Imports.razor"
+#line 11 "C:\Users\Frederik\RiderProjects\Test-Exam-A20\AuthorBlazor\_Imports.razor"
 using Radzen.Blazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "C:\Users\N_i_v\RiderProjects\Test-Exam-A20\AuthorBlazor\_Imports.razor"
+#line 12 "C:\Users\Frederik\RiderProjects\Test-Exam-A20\AuthorBlazor\_Imports.razor"
 using Radzen;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\N_i_v\RiderProjects\Test-Exam-A20\AuthorBlazor\Pages\CreateAuthor.razor"
+#line 2 "C:\Users\Frederik\RiderProjects\Test-Exam-A20\AuthorBlazor\Pages\CreateAuthor.razor"
 using AuthorBlazor.Data;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\N_i_v\RiderProjects\Test-Exam-A20\AuthorBlazor\Pages\CreateAuthor.razor"
+#line 3 "C:\Users\Frederik\RiderProjects\Test-Exam-A20\AuthorBlazor\Pages\CreateAuthor.razor"
 using Microsoft.AspNetCore.Components;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\N_i_v\RiderProjects\Test-Exam-A20\AuthorBlazor\Pages\CreateAuthor.razor"
+#line 4 "C:\Users\Frederik\RiderProjects\Test-Exam-A20\AuthorBlazor\Pages\CreateAuthor.razor"
 using Model;
 
 #line default
@@ -125,7 +125,7 @@ using Model;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 21 "C:\Users\N_i_v\RiderProjects\Test-Exam-A20\AuthorBlazor\Pages\CreateAuthor.razor"
+#line 21 "C:\Users\Frederik\RiderProjects\Test-Exam-A20\AuthorBlazor\Pages\CreateAuthor.razor"
        
     private Author _author;
 
